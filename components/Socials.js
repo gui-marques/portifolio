f8da29181ea2a439fe0889a24cbbc2e51710d33b
gui-marques <gui.slayon@live.com>
2024-03-18 12:09:09 -0300

    1803224

--- components/Socials.js
+++ components/Socials.js
@@ -9,17 +9,17 @@ RiLinkedinLine
 
 const Socials = () => {
   return (
-    <div className='flex items-center gap-x-5 text-lg'>
+    <div className='flex items-center gap-x-5 text-white  text-lg'>
       <Link href={'https://ttsmaker.com/br'} target="_blank" className='hover:text-accent transition-all duration-300'>
       <RiYoutubeLine />
       </Link>
-      <Link href={'https://ttsmaker.com/br'} className='hover:text-accent transition-all duration-300' target="_blank">
+      <Link href={'https://www.instagram.com/dc.socialmidia/'} className='hover:text-accent transition-all duration-300' target="_blank">
       <RiInstagramLine />
       </Link>
       <Link href={'https://ttsmaker.com/br'} className='hover:text-accent transition-all duration-300' target="_blank">
       <RiFacebookLine />
       </Link>
-      <Link href={'https://ttsmaker.com/br'} className='hover:text-accent transition-all duration-300' target="_blank">
+      <Link href={'https://www.linkedin.com/in/d%C3%A9bora-cristina-56341b191/'} className='hover:text-accent transition-all duration-300' target="_blank">
       <RiLinkedinLine />
       </Link>
 
